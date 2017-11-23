fde9548b7bf06a9fda0260c1fa305ba65be94fdb
Ariel Jerez <arieljerez@gmail.com>
2017-11-23 01:48:23 -0300

    date_registration

--- resources/views/date_registration.blade.php
+++ resources/views/date_registration.blade.php
@@ -15,7 +15,7 @@
         <a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled">3</a>
       </div>
       <div class="stepwizard-step col-xs-3 col-md-3">
-        <a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled">4</a>
+        <a href="#step-4" type="button" class="btn btn-default btn-circle" disabled="disabled">4</a>
       </div>
     </div>
 </div>
@@ -66,7 +66,7 @@
                       <i class="fa fa-credit-card fa-2x" aria-hidden="true" style="margin-right:3px"></i>
                       {{$healt_insurance->insurance}}
                     </a>
-                    <button class="btn btn-primary nextBtn pull-right" type="button" >Seleccionar</button>
+                    <span class="btn btn-success btn-sm nextBtn pull-right" type="button"><i class="fa fa-chevron-right" aria-hidden="true"></i> </span>
                   </h5>
                 </div>
 
@@ -98,10 +98,12 @@
       <div class="col-xs-12 col-md-8">
         <div class="col-md-12">
           <h3> Especialidad / Medico </h3>
+          <div class="panel-group" id="especialidad_medico_accordion">
+
             <div class="panel panel-default">
               <div class="panel-heading" role="tab" id="heading-1">
                 <h5 class="panel-title">
-                  <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse-5" aria-expanded="false" aria-controls="collapse-5">
+                  <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#especialidad_medico_accordion" href="#collapse-5" aria-expanded="false" aria-controls="collapse-5">
                     <i class="fa fa-hospital-o fa-2x" aria-hidden="true" style="margin-right:3px"></i>
                     Por Especialidad
                   </a>
@@ -109,20 +111,20 @@
               </div>
 
               <div id="collapse-5" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading-5">
-                <div class="panel-body">
-                  <ul class="list-group">
-                    <li class="list-group-item">Clinica Medica
-                      <button class="label btn-success nextBtn pull-right" type="button">Seleccionar</button>
-                      </li>
-                    <li class="list-group-item">Oftalmologia <span class="label btn-danger pull-right" type="button">sin cobertura</span></li>
-                    <li class="list-group-item">Obstetricia <button class="label btn-success nextBtn pull-right" type="button">Seleccionar</button></li>
-                  </ul>
-                </div>
+                <li class="list-group-item">Clinica Medica
+                  <span class="btn btn-success btn-sm nextBtn pull-right" type="button"><i class="fa fa-chevron-right" aria-hidden="true"></i> </span>
+                </li>
+                <li class="list-group-item">Oftalmologia
+                  <span class="btn btn-danger btn-sm pull-right"><i class="fa fa-ban" aria-hidden="true"></i></span>
+                </li>
+                <li class="list-group-item">Obstetricia
+                  <span class="btn btn-success btn-sm nextBtn pull-right" type="button"><i class="fa fa-chevron-right" aria-hidden="true"></i> </span>
+                </li>
               </div>
 
               <div class="panel-heading" role="tab" id="heading-1">
                 <h5 class="panel-title">
-                  <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse-6" aria-expanded="false" aria-controls="collapse-6">
+                  <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#especialidad_medico_accordion" href="#collapse-6" aria-expanded="false" aria-controls="collapse-6">
                     <i class="fa fa-user-md fa-2x" aria-hidden="true" style="margin-right:3px"></i>
                     Por Médico
                   </a>
@@ -130,40 +132,151 @@
               </div>
 
               <div id="collapse-6" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading-6">
-                <div class="panel-body">
-                  <ul class="list-group">
-                    <li class="list-group-item">Clinica Medica
-                      <button class="label btn-success nextBtn pull-right" type="button">Seleccionar</button>
-                      </li>
-                    <li class="list-group-item">Oftalmologia <span class="label btn-danger pull-right" type="button">sin cobertura</span></li>
-                    <li class="list-group-item">Obstetricia <button class="label btn-success nextBtn pull-right" type="button">Seleccionar</button></li>
 
+                  <ul class="list-group">
+                    <li class="list-group-item">Carlota White
+                      <span class="btn btn-success btn-sm nextBtn pull-right" type="button"><i class="fa fa-chevron-right" aria-hidden="true"></i> </span>
+                    </li>
+                    <li class="list-group-item">Elio Diaz
+                      <span class="btn btn-danger btn-sm pull-right"><i class="fa fa-ban" aria-hidden="true"></i></span>
+                    </li>
+                    <li class="list-group-item">Osvaldo Dorna Del Trebol Perez
+                      <span class="btn btn-success btn-sm nextBtn pull-right" type="button"><i class="fa fa-chevron-right" aria-hidden="true"></i> </span>
+                    </li>
                   </ul>
-                </div>
+
               </div>
 
             </div>
-          <button class="btn btn-primary nextBtn btn-lg pull-right" type="button" >Next</button>
+          </div>
+
+
         </div>
       </div>
     </div>
     <div class="row setup-content" id="step-3">
       <div class="col-xs-12 col-md-8">
         <div class="col-md-12">
-          <h3> Especialidad </h3>
-          <button class="btn btn-success btn-lg pull-right" type="submit">Submit</button>
+            Especialidad / Médico / Fecha
+          <div class="panel-group" id="accordion_especialidad">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h4 class="panel-title">
+                  <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion_especialidad" href="#especialidad_collapse1"><span class="fa fa-hospital-o fa-2x"></span> Clinica Medica</a>
+                </h4>
+              </div>
+              <div id="especialidad_collapse1" class="panel-collapse collapse">
+                  @php
+                    $fechas = array('Lunes','Martes','Miercoles','Jueves','Viernes');
+                  @endphp
+
+                  @foreach ($fechas as $fecha)
+
+                    @php
+                      $label = $loop->index > 0 ?  "success": "danger";
+                      $index = $loop->index;
+                    @endphp
+                      <div class="panel-group">
+                        <div class="panel panel-default">
+                          <div class="panel-heading">
+                            <h4 class="panel-title">
+                              <a data-toggle="collapse" href="#fecha_collapse{{$loop->index}}">
+                                <span aria-hidden="true" class="fa fa-calendar" style="margin-right: 3px;"></span>
+                                2{{$loop->index}}/11/2017 {{$fecha}}</a>
+
+                              <span class="label label-{{$label}}" style="float: right; margin-right: 3px;">{{$loop->index}}</span>
+                            </h4>
+                          </div>
+                          <div id="fecha_collapse{{$loop->index}}" class="panel-collapse collapse">
+                              @if ($index == 0)
+                              <div class="list-group" style="background-color: ligthgrey">
+                                <li class="list-group-item" disabled><i class="fa fa-clock-o" aria-hidden="true"></i> 9:00  <span class="fa fa-user-md"></span> Carlota White </li>
+                                <li class="list-group-item" disabled><i class="fa fa-clock-o" aria-hidden="true"></i> 10:00 <span class="fa fa-user-md"></span> Carlota White </li>
+                                <li class="list-group-item" disabled><i class="fa fa-clock-o" aria-hidden="true"></i> 11:00 <span class="fa fa-user-md"></span> Carlota White </li>
+                              </div>
+                              @elseif($index > 0)
+                                <div class="list-group" style="background-color: ligthgrey">
+                                  <a href="#" class="list-group-item nextBtn"><i class="fa fa-clock-o" aria-hidden="true"></i> 9:00  <span class="fa fa-user-md"></span> Carlota White </a>
+                                  <a href="#" class="list-group-item nextBtn"><i class="fa fa-clock-o" aria-hidden="true"></i> 10:00 <span class="fa fa-user-md"></span> Carlota White </a>
+                                  <a href="#" class="list-group-item nextBtn"><i class="fa fa-clock-o" aria-hidden="true"></i> 11:00 <span class="fa fa-user-md"></span> Carlota White </a>
+                                </div>
+                              @endif
+
+                          </div>
+                        </div>
+                      </div>
+                    @endforeach
+
+              </div>
+            </div>
+
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h4 class="panel-title">
+                  <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion_especialidad" href="#especialidad_obstetricia_collapse1"><span class="fa fa-hospital-o fa-2x"></span> Obstetricia</a>
+                </h4>
+              </div>
+              <div id="especialidad_obstetricia_collapse1" class="panel-collapse collapse">
+
+                  @php
+                    $fechas = array('Lunes','Martes','Miercoles','Jueves','Viernes');
+                  @endphp
+
+                  @foreach ($fechas as $fecha)
+
+                    @php
+                      $label = $loop->index > 0 ?  "success": "danger";
+                      $index = $loop->index;
+                    @endphp
+                      <div class="panel-group">
+                        <div class="panel panel-default">
+                          <div class="panel-heading">
+                            <h4 class="panel-title">
+                              <a data-toggle="collapse" href="#fecha_collapse{{$loop->index}}">
+                                <span aria-hidden="true" class="fa fa-calendar" style="margin-right: 3px;"></span>
+                                2{{$loop->index}}/11/2017 {{$fecha}}</a>
+
+                              <span class="label label-{{$label}}" style="float: right; margin-right: 3px;">{{$loop->index}}</span>
+                            </h4>
+                          </div>
+                          <div id="fecha_collapse{{$loop->index}}" class="panel-collapse collapse">
+                              @if ($index == 0)
+                              <div class="list-group" style="background-color: ligthgrey">
+                                <li class="list-group-item" disabled><i class="fa fa-clock-o" aria-hidden="true"></i> 9:00  <span class="fa fa-user-md"></span> Carlota White </li>
+                                <li class="list-group-item" disabled><i class="fa fa-clock-o" aria-hidden="true"></i> 10:00 <span class="fa fa-user-md"></span> Carlota White </li>
+                                <li class="list-group-item" disabled><i class="fa fa-clock-o" aria-hidden="true"></i> 11:00 <span class="fa fa-user-md"></span> Carlota White </li>
+                              </div>
+                              @elseif($index > 0)
+                                <div class="list-group" style="background-color: ligthgrey">
+                                  <a href="#" class="list-group-item nextBtn"><i class="fa fa-clock-o" aria-hidden="true"></i> 9:00  <span class="fa fa-user-md"></span> Carlota White </a>
+                                  <a href="#" class="list-group-item nextBtn"><i class="fa fa-clock-o" aria-hidden="true"></i> 10:00 <span class="fa fa-user-md"></span> Carlota White </a>
+                                  <a href="#" class="list-group-item nextBtn"><i class="fa fa-clock-o" aria-hidden="true"></i> 11:00 <span class="fa fa-user-md"></span> Carlota White </a>
+                                </div>
+                              @endif
+
+                          </div>
+                        </div>
+                      </div>
+                    @endforeach
+
+
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <div class="row setup-content" id="step-4">
+      <div class="col-xs-12 col-md-8">
+        <div class="col-md-12">
+        <h3> Turno Resevado </h3>
+
         </div>
       </div>
     </div>
 
-    <div class="row setup-content" id="step-4">
-      <div class="col-xs-12 col-md-8">
-        <div class="col-md-12">
-          <h3> Médico </h3>
-          <button class="btn btn-success btn-lg pull-right" type="submit">Submit</button>
-        </div>
-      </div>
-    </div>
+
   </form>
 
 
